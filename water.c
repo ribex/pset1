e738095485e65762cf7e1db7019ca055451e887e
Rebecca Wagaman <ribexy@gmail.com>
2017-11-05 20:23:47 +0000

    Update water.c to check for positive integer input

--- water.c
+++ water.c
@@ -2,9 +2,18 @@
 #include <cs50.h>
 
 int main(void) {
-    printf("Minutes: ");
-    // get user input for an integer of number of minutes
+
+    // Implement a program that reports a user’s water usage, converting minutes spent in the shower to bottles of drinking water.
+
+    int minutes;
+
+    // prompt the user for valid input of a positive int
+    do {
+        printf("Minutes: ");
+        minutes = get_int();
+    } while (minutes < 1);
+
     // multiply by 12 to get the number of bottles
-    int bottles = get_int() * 12;
+    int bottles = minutes * 12;
     printf("Bottles: %i\n", bottles);
 }
