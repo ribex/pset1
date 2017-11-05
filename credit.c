c5a52e6452df89e2ab8bfec53f2353bc58d6db9f
Rebecca Wagaman <ribexy@gmail.com>
2017-11-05 20:31:10 +0000

    Initial commit of credit.c

---
+++ credit.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products' digits together.
+
+    // Add the sum to the sum of the digits that weren’t multiplied by 2.
+
+    //  If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+
+}
